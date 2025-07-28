07b22c66b98a6260dcb29acfa33f6017fa8d1bc7
Terry - Le Quang Nguyen <terry.le>
2025-07-28 20:50:58 +0700

    Clean up files

--- nextjs-client-app/src/components/header/UserDropdown.tsx
+++ nextjs-client-app/src/components/header/UserDropdown.tsx
@@ -3,13 +3,12 @@ import Image from "next/image";
 import React, { useState } from "react";
 import { Dropdown } from "../ui/dropdown/Dropdown";
 import { DropdownItem } from "../ui/dropdown/DropdownItem";
-import { useSession } from "next-auth/react";
-  import { signOut } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 
 export default function UserDropdown() {
   const [isOpen, setIsOpen] = useState(false);
-  const { data: session, update } = useSession()
-  const [name, setName] = useState(`New ${session?.user?.name}`)
+  const { data: session } = useSession(); // <-- get session data
+  const name = session?.user?.name || "None";
 
   function toggleDropdown(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
     e.stopPropagation();
@@ -34,7 +33,7 @@ export default function UserDropdown() {
           />
         </span>
 
-        <span className="block mr-1 font-medium text-theme-sm">Musharof</span>
+        <span className="block mr-1 font-medium text-theme-sm">{name}</span>
 
         <svg
           className={`stroke-gray-500 dark:stroke-gray-400 transition-transform duration-200 ${
@@ -61,15 +60,6 @@ export default function UserDropdown() {
         onClose={closeDropdown}
         className="absolute right-0 mt-[17px] flex w-[260px] flex-col rounded-2xl border border-gray-200 bg-white p-3 shadow-theme-lg dark:border-gray-800 dark:bg-gray-dark"
       >
-        <div>
-          <span className="block font-medium text-gray-700 text-theme-sm dark:text-gray-400">
-            Musharof Chowdhury
-          </span>
-          <span className="mt-0.5 block text-theme-xs text-gray-500 dark:text-gray-400">
-            {/* {session.user.email} */}
-          </span>
-        </div>
-
         <ul className="flex flex-col gap-1 pt-4 pb-3 border-b border-gray-200 dark:border-gray-800">
           <li>
             <DropdownItem
